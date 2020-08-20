3012f0d2c2645c0ded74fca31ea7ec1657f3e630
Teodora <f31189d@mmemea.marelliad.net>
2020-08-20 16:11:17 +0300

    Final version

--- 01_hello_name.py
+++
@@ -1,9 +0,0 @@
-from os import system
-system('clear')
-    while True:
-        name= input('Enter your name: ')
-        if name=='' or name is None:
-            print ('goodbye')
-        break
-    print(f'Hello {name}')
-
