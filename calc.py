7373370ed7b5f674df6baeb3f160f120ba74f020
Teodora <f31189d@mmemea.marelliad.net>
2020-08-20 15:56:26 +0300

    functions implementation complete

--- calc.py
+++ calc.py
@@ -3,15 +3,28 @@
 def add(a, b):
     return a+b
 
-def subs(a, b)
+def subs(a, b):
     return a-b
 
-
-def multiply(a, b)
+def multiply(a, b):
     return a*b
 
-def divide(a, b)
+def divide(a, b):
     return a/b
 
+def root(a):
+    return math.sqrt(a)
 
+def square(a):
+    return a*a
+
+def sin(a):
+    return math.sin(a)
+
+def cos(a):
+    return math.cos(a)
+
+
+
+if __name__ == '__main__'
 
