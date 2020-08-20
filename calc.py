db96e4ec9e9937841c1ccf76e5a571471bab17c2
Teodora <f31189d@mmemea.marelliad.net>
2020-08-20 16:09:29 +0300

    First Version finished

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-
+import math
 
 def add(a, b):
     return a+b
@@ -26,5 +26,18 @@ def cos(a):
 
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
+    while True:
+        a=int(input('a='))
+        b=int(input('b='))
+        print(f'add {a} and {b}={add(a,b)}')
+        print(f'substract {a} and {b}={subs(a,b)}')
+        print(f'multiply {a} and {b}={multiply(a,b)}')
+        print(f'divide {a} and {b}={divide(a,b)}')
+        print(f'root {a} is ={root(a)}')
+        print(f'square {a} is ={square(a)}')
+        print(f'sin {a} is ={sin(a)}')
+        print(f'cos {a} is ={cos(a)}')
+        if a==0 and b==0:
+            break
 
